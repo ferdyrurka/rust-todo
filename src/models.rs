77ada69fd3d6d7f4371a35ce51a2e1f45ec8c549
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-05-11 22:23:32 +0200

    Add NewTodo Model

--- src/models.rs
+++ src/models.rs
@@ -1,6 +1,15 @@
+use schema::todo;
+
 #[derive(Queryable)]
 pub struct Todo {
     pub todo_id: i32,
     pub task: String,
     pub priority: i16
+}
+
+#[derive(Insertable)]
+#[table_name = "todo"]
+pub struct NewTodo<'a> {
+    pub task: &'a str,
+    pub priority: &'a i16
 }
