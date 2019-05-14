c71eb4a3db38101c675d4868a21e9a17c9df2455
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-05-14 21:08:46 +0200

    add find_all in todo_repository

--- src/repository/todo_repository.rs
+++ src/repository/todo_repository.rs
@@ -1,26 +1,27 @@
 use crate::database;
-use crate::models::{NewTodo};
+use crate::models::{NewTodo, Todo};
+use crate::schema::todo::dsl::*;
+use std::vec::Vec;
 use diesel::prelude::*;
 
+pub fn find_all() -> Vec<Todo> {
+    return todo.limit(30)
+        .load::<Todo>(&database::establish_connection())
+        .expect("Error loading todo")
+    ;
+}
+
 pub fn create_task(new_todo: NewTodo) -> bool {
-    use crate::schema::todo::dsl::*;
-
-    let conn = database::establish_connection();
-
     match diesel::insert_into(todo).values(&new_todo)
-        .execute(&conn) {
+        .execute(&database::establish_connection()) {
         Ok(..) => return true,
         Err(..) => return false
     };
 }
 
 pub fn delete_task(todo_id_delete:i32) -> bool {
-    use crate::schema::todo::dsl::*;
-
-    let conn= database::establish_connection();
-
     match diesel::delete(todo.filter(todo_id.eq(todo_id_delete)))
-        .execute(&conn) {
+        .execute(&database::establish_connection()) {
         Ok(..) => return true,
         Err(..) => return false
     };
