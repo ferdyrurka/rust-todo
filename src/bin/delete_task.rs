52ab8bab38516a1f255bc0c64bff13be6ec9e5a1
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-05-14 20:46:18 +0200

    delete_task

---
+++ src/bin/delete_task.rs
@@ -0,0 +1,23 @@
+extern crate rust_todo_cli;
+
+use rust_todo_cli::repository::todo_repository;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let task_id:i32;
+
+    match args[1].trim().parse::<i32>() {
+        Ok(i) => task_id = i,
+        Err(..) => {
+            println!("Task id is not integer!");
+            return;
+        }
+    };
+
+    if !todo_repository::delete_task(task_id) {
+        println!("Failed delete todo!");
+    }
+
+    println!("Delete todo successfully!");
+}
--- src/repository/todo_repository.rs
+++ src/repository/todo_repository.rs
@@ -1,16 +1,27 @@
 use crate::database;
 use crate::models::{NewTodo};
-use diesel::insert_into;
 use diesel::prelude::*;
 
-pub fn create_task(new_todo: &NewTodo) -> bool {
+pub fn create_task(new_todo: NewTodo) -> bool {
     use crate::schema::todo::dsl::*;
 
     let conn = database::establish_connection();
 
-    match insert_into(todo).values(new_todo)
+    match diesel::insert_into(todo).values(&new_todo)
         .execute(&conn) {
-        Ok(_i) => return true,
+        Ok(..) => return true,
+        Err(..) => return false
+    };
+}
+
+pub fn delete_task(todo_id_delete:i32) -> bool {
+    use crate::schema::todo::dsl::*;
+
+    let conn= database::establish_connection();
+
+    match diesel::delete(todo.filter(todo_id.eq(todo_id_delete)))
+        .execute(&conn) {
+        Ok(..) => return true,
         Err(..) => return false
     };
 }
